670cfaaad313fe33985e711e0d12274bafa00d52
Ryan Puyat <ryanspuyat@gmail.com>
2026-01-12 19:31:57 +0900

    add banner carousel for featured product

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
 // import sampleData from '@/db/sample-data';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from '@/lib/actions/product.actions';
 import ProductList from '@/components/shared/product/product-list';
+import ProductCarousel from '@/components/shared/product/product-carousel';
 
 async function Homepage() {
   const latestProduct = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProduct} title="Newest Arrival" limit={4} />
     </>
   );
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,13 +3,15 @@ import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
 // import Menu from './menu';
 import MenuWrapper from './menu-wrapper';
+import CategoryDrawer from './category-drawer';
 
 function Header() {
   return (
     <header className="w-full border-b ">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
               src="/EmartLogo.png"
               alt={`${APP_NAME}`}
