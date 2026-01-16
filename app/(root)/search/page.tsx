67d5552a1a94d29f94874abd7dec24c0b8aa2fdc
Ryan Puyat <ryanspuyat@gmail.com>
2026-01-16 21:17:58 +0900

    rating filter and filter display

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from '@/components/shared/product/product-card';
+import { Button } from '@/components/ui/button';
 import { getAllProducts } from '@/lib/actions/product.actions';
 import { getAllCategories } from '@/lib/actions/product.actions';
 import Link from 'next/link';
@@ -25,6 +26,8 @@ const prices = [
     value: '501-1000',
   },
 ];
+
+const ratings = [4, 3, 2, 1];
 
 async function SearchgPage(props: {
   searchParams: Promise<{
@@ -142,8 +145,51 @@ async function SearchgPage(props: {
             ))}
           </ul>
         </div>
+
+        {/* Rating Links */}
+        <div className="text-xl mb-4 mt-8">Customer Rating</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${rating === 'all' && 'font-bold'}`}
+                href={getFilterUrl({ r: 'all' })}
+              >
+                Any
+              </Link>
+            </li>
+            {ratings.map((r) => (
+              <li key={r}>
+                <Link
+                  className={`${rating === r.toString() && 'font-bold'}`}
+                  href={getFilterUrl({ r: `${r}` })}
+                >
+                  {`${r} stars & up`}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="md:col-span-4 space-y-4">
+        <div className="flex-between flex-col my-4 md:flex-row">
+          <div className="flex items-center">
+            {q !== 'all' && q !== '' && 'Query: ' + q}
+            {category !== 'all' && category !== '' && 'Category: ' + category}
+            {price !== 'all' && ' Price: ' + price}
+            {rating !== 'all' && ' Rating: ' + rating + ' stars & up'}
+            &nbsp;
+            {(q !== 'all' && q !== '') ||
+            (category !== 'all' && category !== '') ||
+            rating !== 'all' ||
+            price !== 'all' ? (
+              <Button variant={'link'} asChild>
+                <Link href="/search">Clear</Link>
+              </Button>
+            ) : null}
+          </div>
+          {/* <div>Sort</div> */}
+        </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products.data.length === 0 && <div>No products found</div>}
           {products.data.map((product) => (
