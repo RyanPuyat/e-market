d1d7292bbabe2e2fc9acd899190feb2959c79dcb
Ryan Puyat <ryanspuyat@gmail.com>
2026-01-28 20:53:15 +0900

    add stripe payment component

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,4 +1,5 @@
-export const APP_SERVER_URL = process.env.NEXT_PUBLIC_APP_SERVER_URL;
+export const APP_SERVER_URL =
+  process.env.NEXT_PUBLIC_APP_SERVER_URL || 'http://localhost:3000';
 export const APP_NAME = 'Emarket';
 export const APP_DESCRIPTION = 'A modern ecommerce website';
 export const LATEST_PRODUCTS_LIMIT =
