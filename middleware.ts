20173138f51bfc94fb06a722d849d5691d1d6a41
Ryan Puyat <ryanspuyat@gmail.com>
2026-01-31 10:33:35 +0900

    middleware test .01

---
+++ middleware.ts
@@ -0,0 +1,81 @@
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+// import { isAuthenticated } from '@/lib/auth-check';
+
+// const protectedRoutes = [
+//   /\/shipping-address/,
+//   /\/payment-method/,
+//   /\/place-order/,
+//   /\/profile/,
+//   /\/user\/(.*)/,
+//   /\/order\/(.*)/,
+//   /\/admin/,
+// ];
+
+// export async function middleware(request: NextRequest) {
+//   const sessionCartId = request.cookies.get('sessionCartId')?.value;
+
+//   if (!sessionCartId) {
+//     const newSessionCartId = crypto.randomUUID();
+
+//     const response = NextResponse.next();
+
+//     response.cookies.set('sessionCartId', newSessionCartId, {
+//       path: '/',
+//       httpOnly: true,
+//       // secure: true,
+//       secure: process.env.NODE_ENV === 'production',
+//       sameSite: 'lax',
+//     });
+
+//     return response;
+//   }
+//   const isProtected = protectedRoutes.some((route) =>
+//     route.test(request.nextUrl.pathname),
+//   );
+
+//   if (isProtected && !(await isAuthenticated(request))) {
+//     return NextResponse.redirect(new URL('/sign-in', request.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { isAuthenticated } from '@/lib/auth-check';
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  const sessionCartId = request.cookies.get('sessionCartId')?.value;
+
+  if (!sessionCartId) {
+    response.cookies.set('sessionCartId', crypto.randomUUID(), {
+      path: '/',
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+    });
+  }
+
+  const isAuth = await isAuthenticated(request);
+
+  if (!isAuth) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    '/shipping-address',
+    '/payment-method',
+    '/place-order',
+    '/profile',
+    '/user/:path*',
+    '/order/:path*',
+    '/admin/:path*',
+  ],
+};
