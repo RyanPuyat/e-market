cee4e25b5825ac0faeb43cd32a732cb7a121e95c
Ryan Puyat <ryanspuyat@gmail.com>
2026-02-16 12:43:30 +0900

    Update resend email functionality

--- app/email/index.tsx
+++ app/email/index.tsx
@@ -10,9 +10,29 @@ const resend = new Resend(process.env.RESEND_API_KEY as string);
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
   await resend.emails.send({
-    from: `${APP_NAME} < ${SENDER_EMAIL} >`,
+    from: `${APP_NAME} <${SENDER_EMAIL}>`,
     to: order.user.email,
     subject: `Order Confirmation ${order.id}`,
     react: <PurchaseReceiptEmail order={order} />,
   });
 };
+
+// import { Resend } from 'resend';
+// import { SENDER_EMAIL, APP_NAME } from '@/lib/constants';
+// import { Order } from '@/types';
+// import PurchaseReceiptEmail from './purchase-receipt';
+
+// const resend = new Resend(process.env.RESEND_API_KEY as string);
+
+// export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
+//   console.log('Sending purchase receipt for order:', order.id);
+
+//   const data = await resend.emails.send({
+//     from: `${APP_NAME} <${SENDER_EMAIL}>`,
+//     to: order.user.email,
+//     subject: `Order Confirmation ${order.id}`,
+//     react: PurchaseReceiptEmail({ order }),
+//   });
+
+//   console.log('Resend response:', data);
+// };
