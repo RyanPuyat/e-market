904a7f3b5a8dd60baa7d25eee0c9229db86897dd
Ryan Puyat <ryanspuyat@gmail.com>
2026-02-17 13:48:31 +0900

    edit auth form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -6,8 +6,8 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: 'ryan@example.com',
-  password: '072587',
+  email: '',
+  password: '',
 };
 
 export const signUpDefaultValues = {
